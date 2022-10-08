1da0c98a071491640c7f68dd4a4c9e818995613c
Yumetsuki <540160575@qq.com>
2022-10-08 20:27:41 +0800

    支持角色定义

--- src/main/kotlin/fan/yumetsuki/yumerpg/core/game/model/Chara.kt
+++ src/main/kotlin/fan/yumetsuki/yumerpg/core/game/model/Chara.kt
@@ -1,9 +1,102 @@
 package fan.yumetsuki.yumerpg.core.game.model
 
-import fan.yumetsuki.yumerpg.core.model.PropertyAbility
-import fan.yumetsuki.yumerpg.core.model.RpgModel
+import fan.yumetsuki.yumerpg.core.model.*
 
-/* 下面实现 PropertyAbility 的能力，为角色拥有的基本属性 */
+/**
+ * 创建一个角色，一个角色是拥有[CharacterAbility]能力的对象
+ * 角色可以是敌人，可以是伙伴，也可以是中立（NPC）
+ * @return [RpgModel]角色对象
+ * @author yumetsuki
+ */
+fun character(
+    // 角色名
+    name: String,
+    characterType: CharacterAbility.CharacterType,
+    abilities: List<RpgAbility<*, *, *, *>>
+) : RpgModel {
+
+    return CommonRpgModel(
+        meta = mapMeta(
+            "name" to name
+        ),
+        abilities = listOf(
+            CharacterAbility(characterType),
+            *abilities.filter {
+                it !is CharacterAbility
+            }.toTypedArray()
+        )
+    )
+
+}
+
+/**
+ * 判断对象是否为角色
+ * @author yumetsuki
+ */
+fun RpgModel.isCharacter(): Boolean = getAbility<CharacterAbility, _, _, _, _>() != null
+
+/**
+ * 判断对象是否为伙伴
+ * @author yumetsuki
+ */
+fun RpgModel.isParty(): Boolean = getAbility<CharacterAbility, _, _, _, _>()?.value == CharacterAbility.CharacterType.Party
+
+/**
+ * 判断对象是否为敌人
+ * @author yumetsuki
+ */
+fun RpgModel.isEnemy(): Boolean = getAbility<CharacterAbility, _, _, _, _>()?.value == CharacterAbility.CharacterType.Enemy
+
+/**
+ * 判断对象是否为 Npc
+ * @author yumetsuki
+ */
+fun RpgModel.isNpc(): Boolean = getAbility<CharacterAbility, _, _, _, _>()?.value == CharacterAbility.CharacterType.Npc
+
+/**
+ * 角色拥有的能力，可以标识自己的特征，是否为伙伴/敌人
+ * @author yumetsuki
+ */
+class CharacterAbility(
+    override var value: CharacterType,
+    override val name: String = "Character",
+    override val alias: String? = "角色"
+): PropertyAbility<CharacterAbility.CharacterType, RpgModel> {
+
+    /**
+     * 角色类型，用于标识角色的特征
+     */
+    class CharacterType(
+        val type: String
+    ) {
+        companion object {
+            /**
+             * 伙伴
+             */
+            val Party = CharacterType("Party")
+
+            /**
+             * 敌人
+             */
+            val Enemy = CharacterType("Enemy")
+
+            /**
+             * Npc
+             */
+            val Npc = CharacterType("Npc")
+
+            /**
+             * 空角色，属于角色定义的范畴，它未在当前游戏中起到任何实际作用，仅仅是一个可供实例化的参考
+             * [CharacterType]为[CharacterType.Empty]的对象不应在游戏对象中出现，仅作为角色类别定义
+             */
+            val Empty = CharacterType("Empty")
+        }
+
+    }
+
+}
+
+/* 下面实现 PropertyAbility 的能力，为角色可以拥有的基本属性 */
 
 /**
  * 等级
