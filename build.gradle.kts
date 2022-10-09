12a0f3cfe1a7fcec62c3f9154eb64ebd5875d279
Yumetsuki <540160575@qq.com>
2022-10-09 10:37:10 +0800

    支持 windows & macOS 的脚本引擎

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 
 plugins {
     kotlin("jvm") version "1.7.10"
@@ -11,8 +12,27 @@ repositories {
     mavenCentral()
 }
 
+fun getJ2v8SupportSystem(): String? {
+    val system = DefaultNativePlatform.getCurrentOperatingSystem()
+    val architecture = DefaultNativePlatform.getCurrentArchitecture()
+    if (system.isWindows) {
+        if (architecture.isI386) {
+            return "win32_x86"
+        }
+        if (architecture.isAmd64) {
+            return "win32_x86_64"
+        }
+    }
+    if (system.isMacOsX) {
+        return "macosx_x86_64"
+    }
+    return null
+}
+
 dependencies {
-    implementation("com.eclipsesource.j2v8:j2v8_win32_x86_64:4.6.0")
+    getJ2v8SupportSystem()?.let { j2v8System ->
+        implementation("com.eclipsesource.j2v8:j2v8_${j2v8System}:4.6.0")
+    } ?: error("仅支持 windows / macOS")
     testImplementation(kotlin("test"))
 }
 
